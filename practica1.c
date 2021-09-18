4c29c9fcc2d278b7d5821375554519e89d9a7522
Jose Luis Bravo <bravo.jose.luis18@gmail.com>
2021-09-18 12:33:47 -0300

    practica 1

--- practica1.c
+++ practica1.c
@@ -6,37 +6,49 @@
 #define INTR 0X1C /* 18.2 Hz */
 void interrupt ( *oldhandler)();
 int count=0;
-float frec;
+int interrupcion;
 
 void interrupt handler()
 {
-/* increase the global counter */
+   /* increase the global counter */
    count++;
-/* call the old routine */
-   oldhandler();
 }
 
 int main(void)
 {
-   printf("indique la frecuencia: ");
-   scanf("%f", &frec);
+   printf("\nIndique el num. de interrupciones x seg: ");
+   scanf("%d", &interrupcion);
+
+   disable();
+   outportb(0x043, 0x00 | 0x00 | 0x36);
+   outportb(0x040, 1193800/interrupcion % 256);
+   outportb(0x040, 1193800/interrupcion / 256);
 
    /* El vector de interrupción es el valor base del segmento y el desplazamiento de la rutina del servicio de interrupción. */
-   oldhandler = getvect(frec); 
+   oldhandler = getvect(INTR); 
 
-/* install the new interrupt handler */
+   /* install the new interrupt handler */
    setvect(INTR, handler);
+   enable();
 
-/* loop until the counter exceeds 20 */
-   while (count < 100) {
-   /*if (count%5 == 0)
-      printf("count is %d\n",count);*/
-   printf("count is %d\n",count);
-   sleep(1);
+   /* loop until the counter exceeds 20 */
+   while (1) {
+      if (count != 0 && count%interrupcion==0){
+            printf("interrupciones en este segundo: %d\n",count); /* interrupciones por segundo*/
+      }
+      if(count>20*interrupcion){   /* luego de 20 segundos */
+         break;
+      }
    }
-
-/* reset the old interrupt handler */
+      
+   disable();
+   /* reset the old interrupt handler */
    setvect(INTR, oldhandler);
+   outportb(0x43, 0x36);
+   outportb(0x40, 0xFF);
+
+   printf("/n PROGRAMA FINALIZADO...");
+   getch();
 
    return 0;
 }
